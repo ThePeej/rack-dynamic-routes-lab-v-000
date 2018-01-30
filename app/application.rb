5c177fbc6e1515e0339dacaf706ccab4fb5d366d
PJ Frias <peejfrias@gmail.com>
2018-01-30 18:54:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,17 @@
 class application
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack.request.new(env)
 
     if req.path.match(/songs/)
       item = req.path.split(/songs/).last
-
+      if @@items.include?(item)
+        resp.write "#{item.price}"
+      else
+        resp.write "Item is not here"
+        resp.status = 400
 
     else
       resp.write "Route not found"
