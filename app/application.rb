ee4979e6c932d0fb7cce8cc3004f0c0c7d561fe4
PJ Frias <peejfrias@gmail.com>
2018-01-30 18:57:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    binding.pry
     if req.path.match(/songs/)
       item = req.path.split(/songs/).last
       binding.pry
