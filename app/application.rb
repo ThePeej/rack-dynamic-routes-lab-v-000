e5f736be8a9daeab52f8fc0750591392e03de613
PJ Frias <peejfrias@gmail.com>
2018-01-30 18:53:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,13 @@
 class application
-
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack.request.new(env)
 
     if req.path.match(/songs/)
       item = req.path.split(/songs/).last
-      
+
 
     else
       resp.write "Route not found"
